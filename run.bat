91b62b1a9d6b22f256e6d89a110d02f86f6c3648
Mohsen Pakzad <44529676+mohsenpakzad@users.noreply.github.com>
2021-01-04 22:14:29 +0330

    Update bat file

--- run.bat
+++ run.bat
@@ -1,4 +1,8 @@
+del AE_Compiler.exe
 bison -d AE_Compiler.y
 flex  AE_Compiler.l
 gcc  -o AE_Compiler lex.yy.c AE_Compiler.tab.c
 .\AE_Compiler.exe
+del lex.yy.c
+del AE_Compiler.tab.h
+del AE_Compiler.tab.c
